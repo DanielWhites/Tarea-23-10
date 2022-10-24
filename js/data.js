15112bf13a5591f8b4a71ae6488c55c059824376
DanielWhites <danielalejandroblanco9@gmail.com>
2022-10-24 01:03:57 -0300

    Primer comentario de esta terea

---
+++ js/data.js
@@ -0,0 +1,37 @@
+const produtcs = [
+  {
+    id: 1,
+    product: "Tostadora",
+    category: "Electrodomestico",
+    brand: "Oster",
+    model: "b10d-s",
+  },
+  {
+    id: 2,
+    product: "Tostiarepa",
+    category: "Electrodomestico",
+    brand: "Oster",
+    model: "IJFV-s",
+  },
+  {
+    id: 3,
+    product: "Nevera",
+    category: "Electrodomestico",
+    brand: "LG",
+    model: "PPOODM-98",
+  },
+  {
+    id: 4,
+    product: "Secaora",
+    category: "Electrodomestico",
+    brand: "Mabe",
+    model: "SDF-98",
+  },
+  {
+    id: 5,
+    product: "Lavadora",
+    category: "Electrodomestico",
+    brand: "Samsung",
+    model: "DFQG-98",
+  },
+];
