9d432c18d765788e2a56775080f6a83d6e7fbca4
DanielWhites <106893268+DanielWhites@users.noreply.github.com>
2022-10-26 22:17:47 -0300

    Add files via upload
    Actualizacion de tarea

---
+++ app.js
@@ -0,0 +1,47 @@
+const myStore =  {
+  let getData = new Promise((resolve, reject) => {
+    let data = produtcs.length;
+  
+    if (data > 0) {
+      resolve(produtcs);
+    } else if (data === 0) {
+      reject({
+        error: "error-3058",
+        message: "No tenemos información para mostrar",
+      });
+    } else {
+      reject({
+        error: "error-3059",
+        message: "Ha ocurrido un error inesperado",
+      });
+    }
+  });
+  
+  getData.then(
+    function (itmes) {
+      let ul = document.createElement("ul");
+      itmes.map((item) => {
+        let li = document.createElement("li");
+        li.innerHTML = `
+        <span class="item">
+        ${item.id}) ${item.product} | ${item.category} | ${item.brand} | ${item.model}
+        </span>`
+        ul.appendChild(li);
+      });
+      document.getElementById("app").appendChild(ul);
+    },
+    function (error) {
+      console.log(error);
+    }
+  );
+};
+
+
+
+const callApi = (data) => {
+  if(data==='miTienda'){
+    mystore();
+  }else{
+    alert("No tengo mercado libre")
+  }
+}
